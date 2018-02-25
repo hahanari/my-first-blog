28be00c9a8664b33f5621e8b0dad9455c25ab13a
hahanari <hahanari@hanmail.net>
2018-02-25 20:04:44 +0900

    Change the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,11 @@ SECRET_KEY = 'dbbuxkp76i&4jjke_o-!02rubblf+u87g2iow#&)tp2&b^fzto'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost',
+                 '127.0.0.1',
+                 '[::1]',
+                 '.pythonanywhere.com',
+                 'hahanari.pythonanywhere.com']
 
 # Application definition
 
